992a99fae3960338b45e0df929f0d1b7f4e8c3fb
softprops <d.tangren@gmail.com>
2015-11-15 02:18:16 -0500

    simple example for now

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,13 @@
 extern crate par;
-extern crate hyper;
-use std::io::Read;
 
 fn main() {
-    /*let bar = par::Bar::new(
-        100, Some("example ")
+    let bar = par::Bar::new(
+        1024*4
     );
-    for _ in 1..101 {
+    for _ in 1..(1024 * 4) {
         bar.incr();
         bar.update();
         std::thread::sleep_ms(10);
-    }*/
-
-    let client = hyper::Client::new();
-    let mut res = client.get("https://github.com/").send().unwrap();
-    let mut buffer = String::new();
-    let bar = par::Bar::new(16763, Some("dl "));
-    let mut reader = par::ProgressReader::new(res, &bar);
-    reader.read_to_string(&mut buffer);
+    }
+    bar.finish_print("done");
 }
